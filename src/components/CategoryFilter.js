a9c8951365027ab48991126c1b44446b8e57ab2c
Precious <preciousmaregwa1997@gmail.com>
2022-05-13 21:22:45 +0300

    complete assigment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, onSetCategory}) {
+
+
+  const allCategories = categories.map((category) => {
+    const isSelected = (category === selectedCategory ? "selected" : null)
+
+    return(
+      <button
+        onClick={() => onSetCategory(category)}
+        key={category}
+        className={isSelected}
+      >
+        {category}
+      </button>
+    );
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {allCategories}
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm( {categories, onAddTask} ) {
+  const [newTask, setNewTask]=useState("")
+
+  const categoriesArr = categories.map((category) => {
+    return(<option key={category}>{category}</option>)
+  })
+
+  function handleSubmit(e){
+    e.preventdefault()
+    onAddTask(newTask)
+  }
+
+
   return (
     <form className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input onChange={(e)=> setNewTask(e.target.value)} type="text" name="text" />
       </label>
       <label>
         Category
         <select name="category">
-          {/* render <option> elements for each category here */}
+          {categoriesArr}
         </select>
       </label>
-      <input type="submit" value="Add task" />
+      <input onSubmit = {handleSubmit} type="submit" value="Add task" />
     </form>
   );
 }
